4abff7750ed741d6a3c8557c45e9a277ffb67d70
tilwa <shahrukhaltaf123@gmail.com>
2024-11-16 22:19:14 +0500

    project3 updated

--- src/resources/projects/projects.js
+++ src/resources/projects/projects.js
@@ -9,6 +9,12 @@ import Dashboard2 from "../images/react/dashboard/dashboard2.png";
 import Dashboard3 from "../images/react/dashboard/dashboard3.png";
 import Dashboard4 from "../images/react/dashboard/dashboard4.png";
 
+// react Travel Todo List
+import travelTodoList1 from "../images/react/travelTodoList/travelTodoList1.png";
+import travelTodoList2 from "../images/react/travelTodoList/travelTodoList2.png";
+import travelTodoList3 from "../images/react/travelTodoList/travelTodoList3.png";
+import travelTodoList4 from "../images/react/travelTodoList/travelTodoList4.png";
+
 //"All", "HTML & CSS", "JavaScript", "ReactJS", "NodeJS"
 export const projects = [
   {
@@ -26,5 +32,18 @@ export const projects = [
     image: Dashboard1,
     images: [Dashboard1, Dashboard2, Dashboard3, Dashboard4],
     link: "https://dashboard-delta-pearl.vercel.app/dashboard",
+  },
+  {
+    id: 3,
+    title: "Travel Todo List",
+    category: "ReactJS",
+    image: travelTodoList4,
+    images: [
+      travelTodoList4,
+      travelTodoList2,
+      travelTodoList3,
+      travelTodoList1,
+    ],
+    link: "https://travel-todo-list-nine.vercel.app/",
   },
 ];
